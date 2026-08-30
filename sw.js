eed035948123bf87623f44cf45050a3b3289c1a7
Finca-Flamenco <elmar.gijs@gmail.com>
2026-08-30 13:19:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-const CACHE = 'finca-guide-v8';
+const CACHE = 'finca-guide-v9';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./header-finca.jpg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))));
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+  if (url.pathname.endsWith('/daily.json')) {
+    event.respondWith(fetch(event.request, { cache: 'no-store' }));
+    return;
+  }
+
   event.respondWith(fetch(event.request).then(response => {
     const copy = response.clone();
     caches.open(CACHE).then(cache => cache.put(event.request, copy));
